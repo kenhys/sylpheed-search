5bec5d942e83fc815eb68f1da8b01ac8854cba44
HAYASHI Kentaro <kenhys@gmail.com>
2012-10-28 19:33:56 +0900

    add import utility

---
+++ src/sylsearch-import.c
@@ -0,0 +1,20 @@
+#include <glib.h>
+#include <glib/gprintf.h>
+#include <gtk/gtk.h>
+
+#include "sylpf_utility.h"
+#include "sylmain.h"
+#include "plugin.h"
+#include "folder.h"
+#include "procmsg.h"
+
+#include "sylsearch.h"
+
+int main(int argc, char *argv[])
+{
+  grn_init();
+
+  grn_fin();
+
+  return 0;
+}
